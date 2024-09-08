7eef85d77796ab4ed856c48f00111fdfcc521c8b
samikshya-22 <samikshya22@gmail.com>
2024-09-08 21:08:58 +0530

    add:new-site

---
+++ front-page.js
@@ -0,0 +1,9 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    // console.log('Learn More button clicked'); 
+    window.location.href = '/about.html'; 
+});
+
+document.getElementById('findCourseBtn').addEventListener('click', function() {
+    // console.log('Find Your Course button clicked'); 
+    window.location.href = 'biswa/index.html';  
+});
